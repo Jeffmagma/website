d5ca5ad4ea9f9bd84155a7fc130daa45e15bfdcf
Jeffmagma <jeffrey.magmachu@gmail.com>
2021-11-23 21:30:21 -0800

    bandage patch over things not showing correctly

--- components/camera_control.js
+++ components/camera_control.js
@@ -13,11 +13,13 @@ export default function CameraControl({ children }) {
 		const half_width = window.innerWidth / 2;
 		const half_height = window.innerHeight / 2;
 		mouse_x = e.pageX;
-		mouse_x -= half_width;
-		mouse_x /= half_width;
 		mouse_y = e.pageY;
+		//console.log("%d %d", mouse_x, mouse_y);
+		mouse_x -= half_width;
+		mouse_x /= parseFloat(half_width);
 		mouse_y -= half_height;
-		mouse_y /= half_height;
+		mouse_y /= parseFloat(half_height);
+		//console.log("%d %d", mouse_x, mouse_y);
 	}
 	// get a reference to a group with the children
 	const ref = useRef();
@@ -29,10 +31,23 @@ export default function CameraControl({ children }) {
 	useFrame(() => {
 		// move the camera based on mouse position
 		camera.position.lerp(vec.set(mouse_x * 2, 0, 11), 0.05);
+		if (isNaN(camera.position.x)) {
+			camera.position.x = 0;
+		}
 		// move children based on mouse position
 		ref.current.position.lerp(vec.set(mouse_x * 1, mouse_y * 0.1, 0), 0.1);
+		if (isNaN(ref.current.position.x)) {
+			ref.current.position.x = 0;
+		}
+		if (isNaN(ref.current.position.y)) {
+			ref.current.position.y = 0;
+		}
 		// rotate children based on mouse position
 		ref.current.rotation.y = THREE.MathUtils.lerp(ref.current.rotation.y, (-mouse_x * Math.PI) / 20, 0.1);
+		if (isNaN(ref.current.rotation.y)) {
+			ref.current.rotation.y = 0;
+		}
+		console.log(ref.current.rotation.y);
 	});
 	// return the children wrapped around a group parent
 	return <group ref={ref}>{children}</group>;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from "../components/header";
 import { Canvas } from "@react-three/fiber";
 import styles from "../styles/index.module.css";
 import Link from "next/link";
+import CameraControl from "../components/camera_control";
 
 export default function Home() {
 	return (
@@ -17,7 +18,8 @@ export default function Home() {
 					<ambientLight />
 					<Header />
 				</Canvas>
-				<div>
+
+				<div style={{ opacity: "50%" }}>
 					<Link href="/page2">
 						<a style={{ backgroundColor: "white" }}>click here</a>
 					</Link>
