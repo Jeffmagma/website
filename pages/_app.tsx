05731287c339d2e978d62b969af82d28c4bdbde2
Jeffmagma <jeffrey.magmachu@gmail.com>
2022-12-21 02:16:41 -0800

    extra typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import {Layout} from "../components/layout";
+import {AppProps} from "next/app";
 
-export default function MyApp({ Component, pageProps }) {
+interface CustomAppProps extends AppProps {
+	Component: AppProps["Component"] & {xc: string}
+}
+
+export default function MyApp({ Component, pageProps }: CustomAppProps) {
 	return (
 		<Layout xc={Component.xc}>
 			<Component {...pageProps}/>
