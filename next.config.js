489791d34993450aad3f0222821025aa7404674f
cyan <jeffrey.magmachu@gmail.com>
2025-07-07 15:13:11 -0400

    trying next config rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        has: [{type: "host", value: "card.cyan.pink"}],
+        destination: "/card"
+      }
+    ];
+  }
 }
