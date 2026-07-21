04ae232c3d927b609bada68b10f1f2fc072c3e2e
cyan <jeffrey.magmachu@gmail.com>
2026-07-20 23:02:41 -0400

    fix: remove catch-all rewrite in next.config.js to let static assets load on card.cyan.pink

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        has: [{type: "host", value: "card.cyan.pink"}],
-        destination: "/card"
-      }
-    ];
-  }
 }
